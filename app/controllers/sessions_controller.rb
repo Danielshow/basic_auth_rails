d442169ddcc8746868a21c5902f1dfc6de33e6c0
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-04-14 16:16:34 +0100

    perform basic authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
- def login
- end 
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+      flash[:notice] = 'You have successfully signed in'
+      sign_in(user)
+    else
+      render 'new'
+    end
+  end
+
+  def delete
+    sign_out
+  end
 end
