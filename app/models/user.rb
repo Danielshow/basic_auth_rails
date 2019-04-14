c540121b73042359454b0df8b989fbacd2bf1afa
Daniel Shotonwa <danielshotonwa53@gmail.com>
2019-04-14 15:26:29 +0100

    use digest_sha to build random numbers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 # Class for model User
 class User < ApplicationRecord
-  validate :email, presence: true
-  validate :name, presence: true, length: { minimum: 3 }
+  before_create :generate_token
+  validates :email, presence: true, unique: true
+  validates :name, presence: true, length: { minimum: 3 }
   has_secure_password
+
+  private
+
+  def generate_token
+    token = Digest::SHA1.hexdigest(SecureRandom.urlsafe_base64.to_s)
+    self.remember_token = token
+  end
 end
